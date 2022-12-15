55ad8eb6d72f9440bbe394f8194156e54b3f0199
CodingStar1016 <sebaschan1016@outlook.com>
2022-12-15 15:45:56 +0700

    change data

--- client/src/books.js
+++ client/src/books.js
@@ -18,10 +18,10 @@ const books = [
     description:
       "The American poet John Shade is dead. His last poem, 'Pale Fire', is put into a book, together with a preface, a lengthy commentary and notes by Shade's editor, Charles Kinbote. Known on campus as the 'Great Beaver', Kinbote is haughty, inquisitive, intolerant, but is he also mad, bad - and even dangerous? As his wildly eccentric annotations slide into the personal and the fantastical, Kinbote reveals perhaps more than he should be.",
     author: "Vladimir Nabokov",
-    price: "14.50",
+    price: "10.69",
     genre: "Fiction",
     rating: 4.7,
-    numReviews: 4,
+    numReviews: 5,
   },
   {
     _id: "3",
@@ -30,7 +30,7 @@ const books = [
     description:
       "One hot spring, the devil arrives in Moscow, accompanied by a retinue that includes a beautiful naked witch and an immense talking black cat with a fondness for chess and vodka. The visitors quickly wreak havoc in a city that refuses to believe in either God or Satan. But they also bring peace to two unhappy Muscovites: one is the Master, a writer pilloried for daring to write a novel about Christ and Pontius Pilate; the other is Margarita, who loves the Master so deeply that she is willing literally to go to hell for him.",
     author: "Mikhail Bulgakov",
-    price: "14.50",
+    price: "14.99",
     genre: "Fiction",
     rating: 4.3,
     numReviews: 4,
@@ -42,7 +42,7 @@ const books = [
     author: "James Joyce",
     description:
       "Loosely based on the Odyssey, this landmark of modern literature follows ordinary Dubliners in 1904. Capturing a single day in the life of Dubliner Leopold Bloom, his friends Buck Mulligan and Stephen Dedalus, his wife Molly, and a scintillating cast of supporting characters, Joyce pushes Celtic lyricism and vulgarity to splendid extremes. Captivating experimental techniques range from interior monologues to exuberant wordplay and earthy humor. A major achievement in 20th century literature.",
-    price: "14.50",
+    price: "1.56",
     genre: "Fiction",
     rating: 3.7,
     numReviews: 4,
@@ -54,7 +54,7 @@ const books = [
     author: "George Orwell",
     description:
       "Among the seminal texts of the 20th century, Nineteen Eighty-Four is a rare work that grows more haunting as its futuristic purgatory becomes more real. Published in 1949, the book offers political satirist George Orwell's nightmarish vision of a totalitarian, bureaucratic world and one poor stiff's attempt to find individuality. The brilliance of the novel is Orwell's prescience of modern life—the ubiquity of television, the distortion of the language—and his ability to construct such a thorough version of hell. Required reading for students since it was published, it ranks among the most terrifying novels ever written.",
-    price: "14.50",
+    price: "7.48",
     genre: "Fiction",
     rating: 4.5,
     numReviews: 4,
@@ -66,19 +66,19 @@ const books = [
     author: "William Gaddis",
     description:
       "The book Jonathan Franzen dubbed the ur-text of postwar fiction and the first great cultural critique, which, even if Heller and Pynchon hadn't read it while composing Catch-22 and V., managed to anticipate the spirit of both”—The Recognitions is a masterwork about art and forgery, and the increasingly thin line between the counterfeit and the fake.",
-    price: "14.50",
+    price: "29.95",
     genre: "Fiction",
     rating: 4.2,
-    numReviews: 4,
+    numReviews: 3,
   },
   {
     _id: "7",
-    name: "The Metamorphosis and Other Stories ",
+    name: "The Metamorphosis and Other Stories",
     image: "/images/7.jpg",
     author: "Franz Kafka",
     description:
       "Virtually unknown during his lifetime, Franz Kafka is now one of the world’s most widely read and discussed authors. His nightmarish novels and short stories have come to symbolize modern man’s anxiety and alienation in a bizarre, hostile, and dehumanized world. This vision is most fully realized in Kafka’s masterpiece, “The Metamorphosis,” a story that is both harrowing and amusing, and a landmark of modern literature. ",
-    price: "14.50",
+    price: "15.00",
     genre: "Fiction",
     rating: 4.4,
     numReviews: 4,
@@ -90,10 +90,10 @@ const books = [
     author: "Franz Kafka",
     description:
       "Virtually unknown during his lifetime, Franz Kafka is now one of the world’s most widely read and discussed authors. His nightmarish novels and short stories have come to symbolize modern man’s anxiety and alienation in a bizarre, hostile, and dehumanized world. This vision is most fully realized in Kafka’s masterpiece, “The Metamorphosis,” a story that is both harrowing and amusing, and a landmark of modern literature. ",
-    price: "14.50",
+    price: "15.94",
     genre: "Fiction",
     rating: 4.4,
-    numReviews: 4,
+    numReviews: 6,
   },
 ];
 
