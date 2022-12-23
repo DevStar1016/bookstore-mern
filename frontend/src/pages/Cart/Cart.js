535c9636108bf78a7c9bd6994e8aa1a3e64c0029
CodingStar1016 <sebaschan1016@outlook.com>
2022-12-23 13:35:16 +0700

    add card page

--- frontend/src/pages/Cart/Cart.js
+++ frontend/src/pages/Cart/Cart.js
@@ -5,10 +5,25 @@ import "./Cart.css";
 const Cart = () => {
   return (
     <div className="cart">
-      <h2>Cart</h2>
-      <Link className="btn checkout-btn" to="/shipping">
-        Thanh toán
-      </Link>
+      <div className="row">
+        <div className="col-md-8">
+          <h2>Cart</h2>
+        </div>
+        <div className="col-md-4">
+          <div className="card checkout-group">
+            <div className="list-group" variant="flush">
+              <div className="list-group-item bg-color">
+                <h2>Sub total (0) Items</h2>
+              </div>
+              <div className="list-group-item bg-color">
+                <Link className="btn checkout-btn" to="/shipping">
+                  Thanh toán
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
