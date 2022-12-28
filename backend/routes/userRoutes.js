19c9fc2c2a09ff4dd8cb4a69fd1a7c52c678f441
CodingStar1016 <sebaschan1016@outlook.com>
2022-12-28 14:36:36 +0700

    add resgiter route and hash password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { authUser, getUserProfile } = require("../controllers/userController");
+const {
+  authUser,
+  registerUser,
+  getUserProfile,
+} = require("../controllers/userController");
 const { project } = require("../middlewares/authMiddleware");
 
+router.post("/", registerUser);
 router.post("/login", authUser);
 router.get("/profile", project, getUserProfile);
 
