c4d9c2f2c734e3782fcd8caf7e3be289525ef277
CodingStar1016 <sebaschan1016@outlook.com>
2022-12-31 14:14:40 +0700

    setup user login

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,17 +1,20 @@
 import {
-  USER_LIST_REQUEST,
-  USER_LIST_SUCCESS,
-  USER_LIST_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
 } from "../constants/userConstants";
 
-export const userListReducer = (state = { users: [] }, action) => {
+export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
-    case USER_LIST_REQUEST:
+    case USER_LOGIN_REQUEST:
       return { loading: true };
-    case USER_LIST_SUCCESS:
-      return { loading: false, users: action.payload };
-    case USER_LIST_FAIL:
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload };
+    case USER_LOGOUT:
+      return {};
     default:
       return state;
   }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,19 +3,26 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { bookListReducer, bookDetailsReducer } from "./reducers/bookReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   bookList: bookListReducer,
   bookDetails: bookDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
