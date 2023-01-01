c5d47938a7c8b749476dd5006bddfc0c19789803
CodingStar1016 <sebaschan1016@outlook.com>
2023-01-01 13:45:43 +0700

    add dropdown and logout

--- frontend/src/components/Header/Navbar.js
+++ frontend/src/components/Header/Navbar.js
@@ -1,19 +1,54 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { logout } from "../../actions/userActions";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/login">Sign In</Link>
-        </li>
         <li>
           <Link to="/cart">
             <i className="fa-sharp fa-solid fa-cart-shopping cart-icon" />
           </Link>
         </li>
+        {userInfo ? (
+          <div class="dropdown" id="username">
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {userInfo.name}
+            </button>
+            <ul class="dropdown-menu">
+              <li>
+                <Link to="/profile" className="dropdown-item">
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <div className="dropdown-item" onClick={logoutHandler}>
+                  Logout
+                </div>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <li>
+            <Link to="/login">Sign In</Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
