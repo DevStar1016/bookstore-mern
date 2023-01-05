83964d25b0359aa9bc113e0f7b289932bc2419ed
CodingStar1016 <sebaschan1016@outlook.com>
2023-01-05 13:52:04 +0700

    add create and update route book

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -39,4 +39,50 @@ const deleteBook = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getBooks, getBookById, deleteBook };
+// @desc    Create a Book
+// @route   POST /api/books
+// @access  Private/Admin
+const createBook = asyncHandler(async (req, res) => {
+  const book = new Book({
+    name: "Test",
+    price: 0,
+    user: req.user._id,
+    image: "/images/8.jpg",
+    author: "godot",
+    genre: "Fiction",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdBook = await book.save();
+  res.status(201).json(createdBook);
+});
+
+// @desc    Update a Book
+// @route   PUT /api/books/:id
+// @access  Private/Admin
+const updateBook = asyncHandler(async (req, res) => {
+  const { name, price, description, image, author, genre, countInStock } =
+    req.body;
+
+  const book = await Book.findById(req.params.id);
+
+  if (book) {
+    book.name = name;
+    book.price = price;
+    book.description = description;
+    book.image = image;
+    book.author = author;
+    book.genre = genre;
+    book.countInStock = countInStock;
+
+    const updatedBook = await book.save();
+    res.json(updatedBook);
+  } else {
+    res.status(404);
+    throw new Error("Book not found");
+  }
+});
+
+module.exports = { getBooks, getBookById, deleteBook, createBook, updateBook };
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -4,11 +4,15 @@ const {
   getBooks,
   getBookById,
   deleteBook,
+  createBook,
+  updateBook,
 } = require("../controllers/bookController");
 const { protect, admin } = require("../middlewares/authMiddleware");
 
 router.get("/", getBooks);
+router.post("/", protect, admin, createBook);
 router.get("/:id", getBookById);
 router.delete("/:id", protect, admin, deleteBook);
+router.put("/:id", protect, admin, updateBook);
 
 module.exports = router;
