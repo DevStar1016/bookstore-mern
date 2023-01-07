4bf9918648dd1cc04bad4f72e4f0065753631a9f
CodingStar1016 <sebaschan1016@outlook.com>
2023-01-07 13:31:22 +0700

    fix upload image

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,10 @@ app.use("/api/books", bookRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/upload", uploadRoutes);
 
-// const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+const dirname = path.resolve();
+app.use("/uploads", express.static(path.join(dirname, "/uploads")));
 
-app.use(notFound);  
+app.use(notFound);
 app.use(errorHandler);
 
 // const PORT = process.env.PORT || 5000;
