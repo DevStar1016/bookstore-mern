77399c51e3297d6c314f7dc3b0529362bd56b3c3
CodingStar1016 <sebaschan1016@outlook.com>
2023-01-25 15:42:34 +0700

    add order route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } = require("../controllers/orderController");
 const { protect } = require("../middlewares/authMiddleware");
 
 router.post("/", protect, addOrderItems);
 router.get("/:id", protect, getOrderById);
+router.put("/:id/pay", protect, updateOrderToPaid);
 
 module.exports = router;
